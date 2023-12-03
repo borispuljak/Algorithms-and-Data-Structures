3dd41d2b480b18e8fa1b9f6e50f08c1466d2cb2a
borispuljak <boris.puljak123@gmail.com>
2023-12-04 00:18:29 +0100

    Vjezba 1 update

---
+++ Vježbe AISP Boris Puljak/Comparables/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Parameters
+{
+    class Program
+    {
+        public static void Swap(ref int a, ref int b)
+        {
+            int temp = a;
+            a = b;
+            b = temp;
+        }
+
+        static void Main(string[] args)
+        {
+            int a = 1, b = 2;
+
+            Console.WriteLine($"a = {a} b = {b}");
+
+            Swap(ref a, ref b);
+
+            Console.WriteLine($"a = {a} b = {b}");
+
+            int[] array = { 2, 3, 4, 1, 8, 6, 5, 7 };
+
+            foreach (int num in array)
+            {
+                Console.Write($"{num}\n");
+            }
+
+            Console.WriteLine();
+
+            Swap(ref array[2], ref array[3]);
+
+            foreach (int num in array)
+            {
+                Console.Write($"{num}\n");
+            }
+        }
+    }
+}
---
+++ Vježbe AISP Boris Puljak/Parameters/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Parameters
+{
+    class Program
+    {
+        static void Main()
+        {
+            Student[] students = {
+            new Student("Ivo", 4.1),
+            new Student("Ana", 4.9),
+            new Student("Iva", 4.3),
+            new Student("Bob", 4.5),
+            new Student("Joe", 4.7)
+        };
+
+            DisplayStudents(students);
+
+            Bubble.Sort(students);
+
+            Console.WriteLine("-----------");
+            DisplayStudents(students);
+        }
+
+        static void DisplayStudents(Student[] students)
+        {
+            foreach (var student in students)
+            {
+                Console.WriteLine(student);
+            }
+        }
+    }
+
+}
